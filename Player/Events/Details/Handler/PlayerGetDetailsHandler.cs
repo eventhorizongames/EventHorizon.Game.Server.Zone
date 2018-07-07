ef136c68d88446a5e8fcc94a3d7008f03d775526
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2018-07-07 18:29:25 -0500

    Debugging: Added logging.

--- Player/Events/Details/Handler/PlayerGetDetailsHandler.cs
+++ Player/Events/Details/Handler/PlayerGetDetailsHandler.cs
@@ -3,19 +3,22 @@ using System.Threading.Tasks;
 using EventHorizon.Game.Server.Core.Player.Connection;
 using EventHorizon.Game.Server.Core.Player.Model;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace EventHorizon.Game.Player.Events.Details.Handler
 {
     public class PlayerGetDetailsHandler : IRequestHandler<PlayerGetDetailsEvent, PlayerDetails>
     {
+        readonly ILogger _logger;
         readonly IPlayerConnectionFactory _connectionFactory;
-        public PlayerGetDetailsHandler(IPlayerConnectionFactory connectionFactory)
+        public PlayerGetDetailsHandler(ILogger<PlayerGetDetailsHandler> logger, IPlayerConnectionFactory connectionFactory)
         {
             _connectionFactory = connectionFactory;
         }
 
         public async Task<PlayerDetails> Handle(PlayerGetDetailsEvent request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("RequestId: ", request.Id);
             var connection = await _connectionFactory.GetConnection();
             var response = await connection.SendAction<PlayerDetails>("GetPlayer", request.Id);
             return response;
