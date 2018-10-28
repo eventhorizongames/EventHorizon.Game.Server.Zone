aa4f7dbcf9cb078654741d434eb4a67b8d6235dd
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2018-10-28 10:00:40 -0500

    Added this[index] to life state.

--- src/EventHorizon.Plugin.Zone.System.Combat/Model/LifeState.cs
+++ src/EventHorizon.Plugin.Zone.System.Combat/Model/LifeState.cs
@@ -16,6 +16,65 @@ namespace EventHorizon.Plugin.Zone.System.Combat.Model
 
         public int Attack { get; set; }
 
+        public object this[string index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case "Condition":
+                        return this.Condition;
+
+                    case "HealthPoints":
+                        return this.HealthPoints;
+                    case "MaxHealthPoints":
+                        return this.MaxHealthPoints;
+
+                    case "ActionPoints":
+                        return this.ActionPoints;
+                    case "MaxActionPoints":
+                        return this.MaxActionPoints;
+
+                    case "Attack":
+                        return this.Attack;
+
+                    default:
+                        return null;
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case "Condition":
+                        this.Condition = (LifeCondition)value;
+                        break;
+
+                    case "HealthPoints":
+                        this.HealthPoints = (int)value;
+                        break;
+                    case "MaxHealthPoints":
+                        this.MaxHealthPoints = (int)value;
+                        break;
+
+                    case "ActionPoints":
+                        this.ActionPoints = (int)value;
+                        break;
+                    case "MaxActionPoints":
+                        this.MaxActionPoints = (int)value;
+                        break;
+
+                    case "Attack":
+                        this.Attack = (int)value;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+
         public static readonly LifeState NEW = new LifeState
         {
             Condition = LifeCondition.ALIVE,
