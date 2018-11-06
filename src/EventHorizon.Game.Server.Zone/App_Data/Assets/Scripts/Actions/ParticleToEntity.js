3be933f79b21561779b3f1047563f55e7f42fe13
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2018-11-05 20:39:32 -0600

    Added logic to Particle to entity client action.

--- src/EventHorizon.Game.Server.Zone/App_Data/Assets/Scripts/Actions/ParticleToEntity.js
+++ src/EventHorizon.Game.Server.Zone/App_Data/Assets/Scripts/Actions/ParticleToEntity.js
@@ -6,5 +6,23 @@
 // - startEntityId: number
 // - endingEntityId: number
 // - particleTemplateId: string 
+// - duration: number
 
-// TODO: Logic for Particle to Entity
+// TODO: Flush this out more.
+const startingEntity = Services.EntityRepository.get(
+    Data.startEntityId
+);
+const endingEntity = Services.EntityRepository.get(
+	Data.endingEntityId
+);
+// Create entity at Starting Location
+const particleEntity = Services.EntityBuilder.createParticleEmitter(
+	"Flame_Particle",
+	startingEntity.position,
+	2
+);
+particleEntity.moveTo(endingEntity.position)
+
+// Remove particleEntity after duration
+setTimeout(() => particleEntity.stop(), 3000);
+setTimeout(() => particleEntity.dispose(), 5000);
