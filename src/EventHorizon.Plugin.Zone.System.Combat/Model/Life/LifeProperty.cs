05476da764e055877352b44090cf79c5643e0076
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2019-02-10 02:48:49 -0600

    Update property casing.

--- src/EventHorizon.Plugin.Zone.System.Combat/Model/Life/LifeProperty.cs
+++ src/EventHorizon.Plugin.Zone.System.Combat/Model/Life/LifeProperty.cs
@@ -2,7 +2,7 @@ namespace EventHorizon.Plugin.Zone.System.Combat.Model.Life
 {
     public struct LifeProperty
     {
-        public static readonly string HP = "HealthPoints";
-        public static readonly string AP = "ActionPoints";
+        public static readonly string HP = "healthPoints";
+        public static readonly string AP = "actionPoints";
     }
 }
