6a1c77331cbeb296088babdcb700277070634be4
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2019-04-21 11:40:46 -0500

    Changed ModelMesh API to just have an AssetId. The AssetId will be used on the client to load the correct model. New system default mesh to be used for new/invalid ModelMesh instances.

--- src/EventHorizon.Plugin.Zone.System.Model/Model/ModelMesh.cs
+++ src/EventHorizon.Plugin.Zone.System.Model/Model/ModelMesh.cs
@@ -6,21 +6,16 @@ namespace EventHorizon.Plugin.Zone.System.Model.Model
         public static readonly ModelMesh DEFAULT = new ModelMesh
         {
             Type = "GLTF",
-            Path = "/Assets/Meshes/",
-            File = "Default_Mesh.glb",
-            HeightOffset = 0,
+            AssetId = "DEFAULT_MESH"
         };
 
         public string Type { get; set; }
-        public string Path { get; set; }
-        public string File { get; set; }
-        public float HeightOffset { get; set; }
+        public string AssetId { get; set; }
 
         public bool IsValid()
         {
             return Type != null
-                && Path != null
-                && File != null;
+                && !string.IsNullOrEmpty(AssetId);
         }
     }
 }
