a645f2a132201b1a0b7260a20abee6137a2338fe
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2019-07-11 06:23:13 +0000

    Merged PR 12: Update Test Project Structure and Reporting
    Updated the ability for the project to work without having to have a single test projects.
    Also updated the reporting to be better.

--- _scripts/test.main.devops.report.ps1
+++ _scripts/test.main.devops.report.ps1
@@ -1,2 +1,8 @@
+param([String]$root="") #Must be the first statement in your script
+
 ## https://medium.com/bluekiri/code-coverage-in-vsts-with-xunit-coverlet-and-reportgenerator-be2a64cd9c2f 
 dotnet reportgenerator "-reports:.\TestResults\Coverage\coverage.cobertura.xml" "-targetdir:.\TestResults\Coverage\Reports" -reportTypes:htmlInline
+
+
+
+dotnet reportgenerator "-reports:../*/TestResults/Coverage.cobertura.xml" "-targetdir:TestResults/Coverage/Reports" "-reporttypes:HtmlInline_AzurePipelines_Dark;Cobertura"
--- _scripts/watch.test.main.ps1
+++ _scripts/watch.test.main.ps1
@@ -1,1 +1,3 @@
-dotnet watch --project ./test/EventHorizon.Game.Server.Zone.Tests test --filter Category!=Performance /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=../lcov.info 
+param([String]$project="./test/EventHorizon.Game.Server.Zone.Tests") #Must be the first statement in your script
+
+dotnet watch --project $project test --filter Category!=Performance /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=../lcov.info 
