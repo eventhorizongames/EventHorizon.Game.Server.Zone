ebc04aa86f0b37da87d81c11501b252e2b97c91b
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-05-03 19:45:37 -0500

    Update devops report script to work on windows.

--- _scripts/test.main.devops.report.ps1
+++ _scripts/test.main.devops.report.ps1
@@ -1,10 +1,13 @@
 param([String]$root="") #Must be the first statement in your script
 
 ## https://medium.com/bluekiri/code-coverage-in-vsts-with-xunit-coverlet-and-reportgenerator-be2a64cd9c2f 
-dotnet reportgenerator "-reports:.\TestResults\Coverage\coverage.cobertura.xml" "-targetdir:.\TestResults\Coverage\Reports" -reportTypes:htmlInline
+# dotnet reportgenerator "-reports:.\TestResults\Coverage\coverage.cobertura.xml" "-targetdir:.\TestResults\Coverage\Reports" -reportTypes:htmlInline
 
 
 
-dotnet reportgenerator "-reports:../*/TestResults/Coverage.cobertura.xml" "-targetdir:TestResults/Coverage/Reports" "-reporttypes:HtmlInline_AzurePipelines_Dark;Cobertura"
+# dotnet reportgenerator "-reports:../*/TestResults/Coverage.cobertura.xml" "-targetdir:TestResults/Coverage/Reports" "-reporttypes:HtmlInline_AzurePipelines_Dark;Cobertura"
 
-dotnet reportgenerator test/EventHorizon.Game.Server.Zone.Tests.Reporter/EventHorizon.Game.Server.Zone.Tests.Reporter.csproj "-reports:../*/TestResults/Coverage.cobertura.xml" "-targetdir:TestResults/Coverage/Reports" "-reporttypes:HtmlInline_AzurePipelines_Dark;Cobertura"
+dotnet test --filter "Category!=Performance & WindowsOnly!=True" /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=.\TestResults\Coverage\
+cd test/EventHorizon.Game.Server.Zone.Tests.Reporter
+dotnet reportgenerator test/EventHorizon.Game.Server.Zone.Tests.Reporter/EventHorizon.Game.Server.Zone.Tests.Reporter.csproj "-reports:../*/TestResults/Coverage/Coverage.cobertura.xml" "-targetdir:TestResults/Coverage/Reports" "-reporttypes:HtmlInline_AzurePipelines_Dark;Cobertura"
+cd ../..
