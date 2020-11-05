7d665d12f402779fd768d23bd3dd08ec9d777025
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-11-04 21:30:06 -0600

    Added Startup Setup for System Admin and Command Plugin
    BED50324-6E14-4A65-B440-74A15890B5D6

---
+++ src/EventHorizon.Zone.System.Admin/Lifetime/OnStartupSetupAdminSystemCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿namespace EventHorizon.Zone.System.Admin.Lifetime
+{
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+    using EventHorizon.Zone.Core.Model.Info;
+    using EventHorizon.Zone.Core.Model.Lifetime;
+    using global::System.IO;
+    using global::System.Threading;
+    using global::System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.Extensions.Logging;
+
+    public class OnStartupSetupAdminSystemCommandHandler
+        : IRequestHandler<OnStartupSetupAdminSystemCommand, OnServerStartupResult>
+    {
+        private readonly ILogger _logger;
+        private readonly IMediator _mediator;
+        private readonly ServerInfo _serverInfo;
+
+        public OnStartupSetupAdminSystemCommandHandler(
+            ILogger<OnStartupSetupAdminSystemCommandHandler> logger,
+            IMediator mediator,
+            ServerInfo serverInfo
+        )
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _serverInfo = serverInfo;
+        }
+
+        public async Task<OnServerStartupResult> Handle(
+            OnStartupSetupAdminSystemCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var path = Path.Combine(
+                _serverInfo.ServerScriptsPath,
+                "Admin"
+            );
+            // Validate Directory Exists
+            if (!await _mediator.Send(
+                new DoesDirectoryExist(
+                    path
+                ),
+                cancellationToken
+            ))
+            {
+                _logger.LogWarning(
+                    "Directory '{DirectoryFullName}' was not found, creating...",
+                    path
+                );
+                await _mediator.Send(
+                    new CreateDirectory(
+                        path
+                    ),
+                    cancellationToken
+                );
+            }
+
+            return new OnServerStartupResult(
+                true
+            );
+        }
+    }
+}
---
+++ test/EventHorizon.Zone.System.Admin.Plugin.Command.Tests/Lifetime/OnStartupSetupAdminCommandsPluginCommandHandlerTests.cs
@@ -0,0 +1,125 @@
+﻿namespace EventHorizon.Zone.System.Admin.Plugin.Command.Tests.Lifetime
+{
+    using global::System.Threading;
+    using global::System.IO;
+    using global::System.Threading.Tasks;
+    using Xunit;
+    using EventHorizon.Zone.Core.Model.Lifetime;
+    using EventHorizon.Zone.System.Admin.Plugin.Command.Lifetime;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using MediatR;
+    using EventHorizon.Zone.Core.Model.Info;
+    using FluentAssertions;
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+
+    public class OnStartupSetupAdminCommandsPluginCommandHandlerTests
+    {
+        [Fact]
+        public async Task ShouldCreateAdminCommandsDirectoryWhenDoesNotExist()
+        {
+            // Given
+            var adminPath = "admin-path";
+            var commandsPath = Path.Combine(
+                adminPath,
+                "Commands"
+            );
+            var expected = new CreateDirectory(
+                commandsPath
+            );
+
+            var loggerMock = new Mock<ILogger<OnStartupSetupAdminCommandsPluginCommandHandler>>();
+            var mediatorMock = new Mock<IMediator>();
+            var serverInfoMock = new Mock<ServerInfo>();
+
+            serverInfoMock.Setup(
+                mock => mock.AdminPath
+            ).Returns(
+                adminPath
+            );
+
+            // When
+            var handler = new OnStartupSetupAdminCommandsPluginCommandHandler(
+                loggerMock.Object,
+                mediatorMock.Object,
+                serverInfoMock.Object
+            );
+            var actual = await handler.Handle(
+                new OnStartupSetupAdminCommandsPluginCommand(),
+                CancellationToken.None
+            );
+
+            // Then
+            actual.Should().Be(
+                new OnServerStartupResult(
+                    true
+                )
+            );
+
+            mediatorMock.Verify(
+                mock => mock.Send(
+                    expected,
+                    CancellationToken.None
+                )
+            );
+        }
+
+        [Fact]
+        public async Task ShouldNotCreateDirectoryWhenAlreadyExisting()
+        {
+            // Given
+            var adminPath = "admin-path";
+            var commandsPath = Path.Combine(
+                adminPath,
+                "Commands"
+            );
+
+            var loggerMock = new Mock<ILogger<OnStartupSetupAdminCommandsPluginCommandHandler>>();
+            var mediatorMock = new Mock<IMediator>();
+            var serverInfoMock = new Mock<ServerInfo>();
+
+            serverInfoMock.Setup(
+                mock => mock.AdminPath
+            ).Returns(
+                adminPath
+            );
+
+            mediatorMock.Setup(
+                mock => mock.Send(
+                    new DoesDirectoryExist(
+                        commandsPath
+                    ),
+                    CancellationToken.None
+                )
+            ).ReturnsAsync(
+                true
+            );
+
+            // When
+            var handler = new OnStartupSetupAdminCommandsPluginCommandHandler(
+                loggerMock.Object,
+                mediatorMock.Object,
+                serverInfoMock.Object
+            );
+            var actual = await handler.Handle(
+                new OnStartupSetupAdminCommandsPluginCommand(),
+                CancellationToken.None
+            );
+
+            // Then
+            actual.Should().Be(
+                new OnServerStartupResult(
+                    true
+                )
+            );
+
+            mediatorMock.Verify(
+                mock => mock.Send(
+                    It.IsAny<CreateDirectory>(),
+                    CancellationToken.None
+                ),
+                Times.Never()
+            );
+        }
+    }
+}
