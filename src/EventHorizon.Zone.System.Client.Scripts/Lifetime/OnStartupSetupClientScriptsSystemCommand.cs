43826c726e2a03bfac894e3853a52daa6993ec91
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-11-05 20:50:05 -0600

    Added Startup Setup for Client Scripts System
    DE67A9FE-5D54-4757-8944-9CB98DF1E6AF

---
+++ src/EventHorizon.Zone.System.Client.Scripts/Lifetime/OnStartupSetupClientScriptsSystemCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace EventHorizon.Zone.System.Client.Scripts.Lifetime
+{
+    using EventHorizon.Zone.Core.Events.Lifetime;
+
+    public struct OnStartupSetupClientScriptsSystemCommand
+        : OnServerStartupCommand
+    {
+    }
+}
---
+++ src/EventHorizon.Zone.System.Client.Scripts/Lifetime/OnStartupSetupClientScriptsSystemCommandHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace EventHorizon.Zone.System.Client.Scripts.Lifetime
+{
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+    using EventHorizon.Zone.Core.Model.Info;
+    using EventHorizon.Zone.Core.Model.Lifetime;
+    using global::System.Threading;
+    using global::System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.Extensions.Logging;
+
+    public class OnStartupSetupClientScriptsSystemCommandHandler
+        : IRequestHandler<OnStartupSetupClientScriptsSystemCommand, OnServerStartupResult>
+    {
+        private readonly ILogger _logger;
+        private readonly IMediator _mediator;
+        private readonly ServerInfo _serverInfo;
+
+        public OnStartupSetupClientScriptsSystemCommandHandler(
+            ILogger<OnStartupSetupClientScriptsSystemCommandHandler> logger,
+            IMediator mediator,
+            ServerInfo serverInfo
+        )
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _serverInfo = serverInfo;
+        }
+
+        public async Task<OnServerStartupResult> Handle(
+            OnStartupSetupClientScriptsSystemCommand request, 
+            CancellationToken cancellationToken
+        )
+        {
+            // Validate Directory Exists
+            if (!await _mediator.Send(
+                new DoesDirectoryExist(
+                    _serverInfo.ClientScriptsPath
+                ),
+                cancellationToken
+            ))
+            {
+                _logger.LogWarning(
+                    "Directory '{DirectoryFullName}' was not found, creating...",
+                    _serverInfo.ClientScriptsPath
+                );
+                await _mediator.Send(
+                    new CreateDirectory(
+                        _serverInfo.ClientScriptsPath
+                    ),
+                    cancellationToken
+                );
+            }
+
+            return new OnServerStartupResult(
+                true
+            );
+        }
+    }
+}
