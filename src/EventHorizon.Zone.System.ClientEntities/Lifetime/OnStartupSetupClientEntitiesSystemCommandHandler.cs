350ff973e8ad43725aa3ddfde6769e9c9fd1bc37
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-11-05 20:16:08 -0600

    Added Startup Setup for ClientEntities System
    8860190E-408A-4D55-A69F-8A1361963E81

---
+++ src/EventHorizon.Zone.System.ClientEntities/Lifetime/OnStartupSetupClientEntitiesSystemCommandHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace EventHorizon.Zone.System.ClientEntities.Lifetime
+{
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+    using EventHorizon.Zone.Core.Model.Info;
+    using EventHorizon.Zone.Core.Model.Lifetime;
+    using global::System.Threading;
+    using global::System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.Extensions.Logging;
+
+    public class OnStartupSetupClientEntitiesSystemCommandHandler
+        : IRequestHandler<OnStartupSetupClientEntitiesSystemCommand, OnServerStartupResult>
+    {
+        private readonly ILogger _logger;
+        private readonly IMediator _mediator;
+        private readonly ServerInfo _serverInfo;
+
+        public OnStartupSetupClientEntitiesSystemCommandHandler(
+            ILogger<OnStartupSetupClientEntitiesSystemCommandHandler> logger,
+            IMediator mediator,
+            ServerInfo serverInfo
+        )
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _serverInfo = serverInfo;
+        }
+
+        public async Task<OnServerStartupResult> Handle(
+            OnStartupSetupClientEntitiesSystemCommand request, 
+            CancellationToken cancellationToken
+        )
+        {
+            // Validate Directory Exists
+            if (!await _mediator.Send(
+                new DoesDirectoryExist(
+                    _serverInfo.ClientEntityPath
+                ),
+                cancellationToken
+            ))
+            {
+                _logger.LogWarning(
+                    "Directory '{DirectoryFullName}' was not found, creating...",
+                    _serverInfo.ClientEntityPath
+                );
+                await _mediator.Send(
+                    new CreateDirectory(
+                        _serverInfo.ClientEntityPath
+                    ),
+                    cancellationToken
+                );
+            }
+
+            return new OnServerStartupResult(
+                true
+            );
+        }
+    }
+}
---
+++ test/EventHorizon.Zone.System.ClientEntities.Tests/Lifetime/OnStartupSetupClientEntitiesSystemCommandHandlerTests.cs
@@ -0,0 +1,116 @@
+﻿namespace EventHorizon.Zone.System.ClientEntities.Tests.Lifetime
+{
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+    using EventHorizon.Zone.Core.Model.Info;
+    using EventHorizon.Zone.Core.Model.Lifetime;
+    using EventHorizon.Zone.System.ClientEntities.Lifetime;
+    using FluentAssertions;
+    using global::System.Threading;
+    using global::System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using Xunit;
+
+    public class OnStartupSetupClientEntitiesSystemCommandHandlerTests
+    {
+        [Fact]
+        public async Task ShouldCreateDirectoryWhenDoesNotExist()
+        {
+            // Given
+            var clientEntityPath = "client-entity-path";
+            var expected = new CreateDirectory(
+                clientEntityPath
+            );
+
+            var loggerMock = new Mock<ILogger<OnStartupSetupClientEntitiesSystemCommandHandler>>();
+            var mediatorMock = new Mock<IMediator>();
+            var serverInfoMock = new Mock<ServerInfo>();
+
+            serverInfoMock.Setup(
+                mock => mock.ClientEntityPath
+            ).Returns(
+                clientEntityPath
+            );
+
+            // When
+            var handler = new OnStartupSetupClientEntitiesSystemCommandHandler(
+                loggerMock.Object,
+                mediatorMock.Object,
+                serverInfoMock.Object
+            );
+            var actual = await handler.Handle(
+                new OnStartupSetupClientEntitiesSystemCommand(),
+                CancellationToken.None
+            );
+
+            // Then
+            actual.Should().Be(
+                new OnServerStartupResult(
+                    true
+                )
+            );
+
+            mediatorMock.Verify(
+                mock => mock.Send(
+                    expected,
+                    CancellationToken.None
+                )
+            );
+        }
+
+        [Fact]
+        public async Task ShouldNotCreateDirectoryWhenAlreadyExisting()
+        {
+            // Given
+            var clientEntityPath = "client-entity-path";
+
+            var loggerMock = new Mock<ILogger<OnStartupSetupClientEntitiesSystemCommandHandler>>();
+            var mediatorMock = new Mock<IMediator>();
+            var serverInfoMock = new Mock<ServerInfo>();
+
+            serverInfoMock.Setup(
+                mock => mock.ClientEntityPath
+            ).Returns(
+                clientEntityPath
+            );
+
+            mediatorMock.Setup(
+                mock => mock.Send(
+                    new DoesDirectoryExist(
+                        clientEntityPath
+                    ),
+                    CancellationToken.None
+                )
+            ).ReturnsAsync(
+                true
+            );
+
+            // When
+            var handler = new OnStartupSetupClientEntitiesSystemCommandHandler(
+                loggerMock.Object,
+                mediatorMock.Object,
+                serverInfoMock.Object
+            );
+            var actual = await handler.Handle(
+                new OnStartupSetupClientEntitiesSystemCommand(),
+                CancellationToken.None
+            );
+
+            // Then
+            actual.Should().Be(
+                new OnServerStartupResult(
+                    true
+                )
+            );
+
+            mediatorMock.Verify(
+                mock => mock.Send(
+                    It.IsAny<CreateDirectory>(),
+                    CancellationToken.None
+                ),
+                Times.Never()
+            );
+        }
+    }
+}
