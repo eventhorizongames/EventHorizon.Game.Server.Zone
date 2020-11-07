9360693e82e4d4cfa94dfb184e53d506a1d53519
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-11-06 22:01:08 -0600

    Added Startup Setup for EntityModule System
    B61C7DEF-6F10-4CC3-81C4-AF2099EC726B

---
+++ src/EventHorizon.Zone.System.EntityModule/Lifetime/OnStartupSetupEntityModuleSystemCommandHandler.cs
@@ -0,0 +1,84 @@
+﻿namespace EventHorizon.Zone.System.EntityModule.Lifetime
+{
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+    using EventHorizon.Zone.Core.Model.Info;
+    using EventHorizon.Zone.Core.Model.Lifetime;
+    using global::System.IO;
+    using global::System.Threading;
+    using global::System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.Extensions.Logging;
+
+    public class OnStartupSetupEntityModuleSystemCommandHandler
+        : IRequestHandler<OnStartupSetupEntityModuleSystemCommand, OnServerStartupResult>
+    {
+        private readonly ILogger _logger;
+        private readonly IMediator _mediator;
+        private readonly ServerInfo _serverInfo;
+
+        public OnStartupSetupEntityModuleSystemCommandHandler(
+            ILogger<OnStartupSetupEntityModuleSystemCommandHandler> logger,
+            IMediator mediator,
+            ServerInfo serverInfo
+        )
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _serverInfo = serverInfo;
+        }
+
+        public async Task<OnServerStartupResult> Handle(
+            OnStartupSetupEntityModuleSystemCommand request, 
+            CancellationToken cancellationToken
+        )
+        {
+            await CreateDirectory(
+                Path.Combine(
+                    _serverInfo.ClientPath,
+                    "Modules",
+                    "Base"
+                ),
+                cancellationToken
+            );
+            await CreateDirectory(
+                Path.Combine(
+                    _serverInfo.ClientPath,
+                    "Modules",
+                    "Player"
+                ),
+                cancellationToken
+            );
+
+            return new OnServerStartupResult(
+                true
+            );
+        }
+
+        private async Task CreateDirectory(
+            string directory, 
+            CancellationToken cancellationToken
+        )
+        {
+
+            // Validate Directory Exists
+            if (!await _mediator.Send(
+                new DoesDirectoryExist(
+                    directory
+                ),
+                cancellationToken
+            ))
+            {
+                _logger.LogWarning(
+                    "Directory '{DirectoryFullName}' was not found, creating...",
+                    directory
+                );
+                await _mediator.Send(
+                    new CreateDirectory(
+                        directory
+                    ),
+                    cancellationToken
+                );
+            }
+        }
+    }
+}
