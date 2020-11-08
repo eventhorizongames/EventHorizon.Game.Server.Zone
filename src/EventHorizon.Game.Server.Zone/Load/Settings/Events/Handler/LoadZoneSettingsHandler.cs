4b1158454ed4be2eac71ca8ec334893a5604bdeb
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-11-07 19:43:43 -0600

    Added Zone Settings creation when Not found during Load.
    E0333F2C-BD8A-4999-943B-D7A05A0E022B

--- src/EventHorizon.Game.Server.Zone/Load/Settings/Events/Handler/LoadZoneSettingsHandler.cs
+++ src/EventHorizon.Game.Server.Zone/Load/Settings/Events/Handler/LoadZoneSettingsHandler.cs
@@ -1,38 +1,43 @@
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using EventHorizon.Game.Server.Zone.Settings.Load;
-using EventHorizon.Zone.Core.Events.FileService;
-using EventHorizon.Zone.Core.Model.Info;
-using EventHorizon.Zone.Core.Model.Json;
-using EventHorizon.Zone.Core.Model.Settings;
-using MediatR;
-using Microsoft.Extensions.Logging;
-using IOPath = System.IO.Path;
-
 namespace EventHorizon.Game.Server.Zone.Load.Settings.Events.Handler
 {
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EventHorizon.Game.Server.Zone.Settings.Load;
+    using EventHorizon.Zone.Core.Events.FileService;
+    using EventHorizon.Zone.Core.Model.Info;
+    using EventHorizon.Zone.Core.Model.Json;
+    using EventHorizon.Zone.Core.Model.Settings;
+    using MediatR;
+    using Microsoft.Extensions.Logging;
+
     public class LoadZoneSettingsHandler : INotificationHandler<LoadZoneSettingsEvent>
     {
-        readonly IMediator _mediator;
-        readonly ILogger _logger;
-        readonly ServerInfo _serverInfo;
-        readonly IJsonFileLoader _fileLoader;
-        readonly IZoneSettingsSetter _zoneSettingsBuilder;
+        private readonly IMediator _mediator;
+        private readonly ILogger _logger;
+        private readonly ServerInfo _serverInfo;
+        private readonly IJsonFileLoader _fileLoader;
+        private readonly IJsonFileSaver _fileSaver;
+        private readonly IZoneSettingsSetter _zoneSettingsBuilder;
+        private readonly IZoneSettingsFactory _zoneSettingsFactory;
 
         public LoadZoneSettingsHandler(
             ILogger<LoadZoneSettingsHandler> logger,
             IMediator mediator,
             ServerInfo serverInfo,
             IJsonFileLoader fileLoader,
-            IZoneSettingsSetter zoneSettingsBuilder
+            IJsonFileSaver fileSaver,
+            IZoneSettingsSetter zoneSettingsBuilder,
+            IZoneSettingsFactory zoneSettingsFactory
         )
         {
             _mediator = mediator;
             _logger = logger;
             _serverInfo = serverInfo;
             _fileLoader = fileLoader;
+            _fileSaver = fileSaver;
             _zoneSettingsBuilder = zoneSettingsBuilder;
+            _zoneSettingsFactory = zoneSettingsFactory;
         }
 
         public async Task Handle(
@@ -51,6 +56,11 @@ namespace EventHorizon.Game.Server.Zone.Load.Settings.Events.Handler
                     "Settings file not found. {SettingsFile}",
                     settingsFile
                 );
+                await _fileSaver.SaveToFile(
+                    Path.GetDirectoryName(settingsFile),
+                    Path.GetFileName(settingsFile),
+                    _zoneSettingsFactory.Settings
+                );
                 return;
             }
             _zoneSettingsBuilder.Set(
@@ -62,7 +72,7 @@ namespace EventHorizon.Game.Server.Zone.Load.Settings.Events.Handler
 
         private string GetSettingsFileName()
         {
-            return IOPath.Combine(
+            return Path.Combine(
                 _serverInfo.AppDataPath,
                 "ZoneSettings.json"
             );
