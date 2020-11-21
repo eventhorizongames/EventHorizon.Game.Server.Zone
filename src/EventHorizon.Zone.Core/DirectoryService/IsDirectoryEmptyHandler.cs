034e947d3ced85c9d0affc5e8a222a068f70f448
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-11-20 22:08:07 -0600

    Added missing IsDirectoryEmpty Handler.

---
+++ src/EventHorizon.Zone.Core/DirectoryService/IsDirectoryEmptyHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace EventHorizon.Zone.Core.DirectoryService
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+    using EventHorizon.Zone.Core.Model.DirectoryService;
+    using MediatR;
+
+    public class IsDirectoryEmptyHandler
+        : IRequestHandler<IsDirectoryEmpty, bool>
+    {
+        private readonly DirectoryResolver _directoryResolver;
+
+        public IsDirectoryEmptyHandler(
+            DirectoryResolver directoryResolver
+        )
+        {
+            _directoryResolver = directoryResolver;
+        }
+
+        public Task<bool> Handle(
+            IsDirectoryEmpty request,
+            CancellationToken cancellationToken
+        ) => _directoryResolver.IsEmpty(
+            request.DirectoryFullName
+        ).FromResult();
+    }
+}
---
+++ test/EventHorizon.Zone.Core.Tests/DirectoryService/IsDirectoryEmptyHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿namespace EventHorizon.Zone.Core.Tests.DirectoryService
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EventHorizon.Zone.Core.DirectoryService;
+    using EventHorizon.Zone.Core.Events.DirectoryService;
+    using EventHorizon.Zone.Core.Model.DirectoryService;
+    using FluentAssertions;
+    using Moq;
+    using Xunit;
+
+    public class IsDirectoryEmptyHandlerTests
+    {
+        [Fact]
+        public async Task ShouldReturnTrueWhenDirectorFullNameInRequest()
+        {
+            // Given
+            var directoryFullName = "directory-full-name";
+            var expected = true;
+
+            var directoryResolverMock = new Mock<DirectoryResolver>();
+
+            directoryResolverMock.Setup(
+                mock => mock.IsEmpty(
+                    directoryFullName
+                )
+            ).Returns(
+                true
+            );
+
+            // When
+            var handler = new IsDirectoryEmptyHandler(
+                directoryResolverMock.Object
+            );
+            var actual = await handler.Handle(
+                new IsDirectoryEmpty(
+                    directoryFullName
+                ),
+                CancellationToken.None
+            );
+
+            // Then
+            actual.Should().Be(
+                expected
+            );
+            directoryResolverMock.Verify(
+                mock => mock.IsEmpty(
+                    directoryFullName
+                )
+            );
+        }
+    }
+}
