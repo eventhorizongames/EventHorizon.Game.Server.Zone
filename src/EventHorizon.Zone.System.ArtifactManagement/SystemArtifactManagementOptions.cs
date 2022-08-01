58eaebccccefef8123fe3ca2a2337099cb6b56b5
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2022-07-31 20:40:46 -0500

    fix: Fixed upload allowed domain list not getting correctly set from appsettings.

--- src/EventHorizon.Zone.System.ArtifactManagement/SystemArtifactManagementOptions.cs
+++ src/EventHorizon.Zone.System.ArtifactManagement/SystemArtifactManagementOptions.cs
@@ -2,15 +2,7 @@
 
 using System.Collections.Generic;
 
-using EventHorizon.Zone.System.ArtifactManagement.Model;
-
-public struct SystemArtifactManagementOptions
+public class SystemArtifactManagementOptions
 {
-    public List<string> AllowedDomainList { get; set; } =
-        new List<string> { ArtifactManagementSystemContants.ALL_DOMAINS, };
-
-    public SystemArtifactManagementOptions(List<string> allowedDomainList)
-    {
-        AllowedDomainList = allowedDomainList;
-    }
+    public List<string> AllowedDomainList { get; set; } = new();
 }
